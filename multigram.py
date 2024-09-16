504cb5c1bb26d964be55c697469f549eb50842b0
Antonio Cuni <anto.cuni@gmail.com>
2024-09-16 17:52:01 +0200

    add a screenshot command

--- multigram.py
+++ multigram.py
@@ -12,20 +12,35 @@ options:
 Available commands:
     list                        list all available acconuts
     start                       launch a new telegram instance
+    screenshot                  take a screenshot of the telegram window
 """
 
 import sys
 import os
 import time
 import shlex
+from dataclasses import dataclass
 from pathlib import Path
 from docopt import docopt
+import pyautogui
 from wmctrl import Window
+
+@dataclass
+class Rect:
+    x: int
+    y: int
+    w: int
+    h: int
+    def as_tuple(self):
+        return (self.x, self.y, self.w, self.h)
 
 TELEGRAM = '/opt/Telegram/Telegram'
 ROOT = Path(__file__).parent
 ACCOUNTS = ROOT.joinpath('accounts')
 ACCOUNTS.mkdir(exist_ok=True)
+
+TELEGRAM_RECT = Rect(x=64, y=0, w=1000, h=1500)
+
 
 def launch_command(*args, bg=False, redirect_null=False):
     cmdline = [shlex.quote(arg) for arg in args]
@@ -44,6 +59,8 @@ def main():
         cmd_list(options)
     elif command == 'start':
         cmd_start(options)
+    elif command == 'screenshot':
+        cmd_screnshot(options)
     elif command == 'dev':
         cmd_dev(options)
     else:
@@ -85,7 +102,6 @@ def cmd_start(options):
                    redirect_null=True)
     reposition_telegram()
 
-
 def reposition_telegram():
     """
     Wait until the active window is a telegram one, then reposition it to a
@@ -104,8 +120,18 @@ def reposition_telegram():
             print('[WINDOW] telegram not found, aborting')
             sys.exit(1)
 
-    w = find_window()
-    w.resize_and_move(x=64, y=0, w=1000, h=1500)
+    win = find_window()
+    R = TELEGRAM_RECT
+    win.resize_and_move(x=R.x, y=R.y, w=R.w, h=R.h)
+
+def cmd_screnshot(options):
+    telegram_screenshot('/tmp/telegram.png')
+
+def telegram_screenshot(filename=None):
+    R = TELEGRAM_RECT
+    os.system(f'import -window root -crop {R.w}x{R.h}+{R.x}+{R.y} {filename}')
+    if filename:
+        print(f'[SCREENSHOT] saved to {filename}')
 
 
 if __name__ == '__main__':
